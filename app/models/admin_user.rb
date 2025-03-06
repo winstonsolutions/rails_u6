58f2642bd64f30ea4ca37a6f953cef086f7b62b2
winston <andyredjohn@gmail.com>
2025-03-06 08:02:43 -0600

    update admin

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,11 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  # Add this method for Ransack
+  def self.ransackable_attributes(auth_object = nil)
+    [ "created_at", "email", "id", "updated_at" ]
+  end
 end
